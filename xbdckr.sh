d95554ca532c38cbd14f1b6952464f7f3b3025fd
/OnnoYukihiro <admin@onnoyukihiro.site>
2022-10-18 09:32:21 +0700

    Update xbdckr.sh

--- xbdckr.sh
+++ xbdckr.sh
@@ -1,2 +1,2 @@
-docker run -it -p 8080:6080 --shm-size 1g --privileged --cap-add=NET_ADMIN thuonghai2711/kali-novnc-v2:latest
+docker run -it -p 8060:6080 --shm-size 1g --privileged --cap-add=NET_ADMIN thuonghai2711/kali-novnc-v2:latest
 sleep 99999999
